d8395b6a2f671e4f95a24cde8fff77edcd386e99
Jorge Luis <jorgefloriano1646@gmail.com>
2025-03-22 20:28:40 -0300

    Create Migrations

--- database/migrations/2025_03_22_230436_create_wishes_table.php
+++ database/migrations/2025_03_22_230436_create_wishes_table.php
@@ -11,36 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('properties', function (Blueprint $table) {
+        Schema::create('wishes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('client_id')->constrained();
             $table->foreignId('district_id')->constrained();
-            $table->string('iptu')->nullable();
-            $table->string('description')->nullable();
-            $table->integer('price');
-            $table->float('land_area')->nullable();
-            $table->float('building_area')->nullable();
-            $table->string('image')->nullable();
-            $table->string('address')->nullable();
             $table->tinyInteger('rooms')->nullable();
             $table->tinyInteger('bathrooms')->nullable();
             $table->tinyInteger('suites')->nullable();
             $table->tinyInteger('garages')->nullable();
-            $table->tinyInteger('floor')->nullable();
-            $table->tinyInteger('building_floors')->nullable();
-            $table->tinyInteger('property_floors')->nullable();
             $table->date('delivery_key')->nullable();
             $table->integer('min_act')->nullable();
-            // entrada parcelada
             $table->boolean('installment_payment')->default(false);
-            $table->boolean('incc_financing')->nullable();
-            $table->boolean('documents')->nullable();
-            $table->string('finsh_type')->nullable();
             $table->enum('air_conditioning', [
                 'incluso',
                 'somente infra',
-                'não incluso',
-            ])->default('não incluso');
+                'não incluso'
+                ])
+                ->default('não incluso');
             $table->boolean('garden')->nullable();
             $table->boolean('pool')->nullable();
             $table->boolean('balcony')->nullable(); // varanda
@@ -56,6 +43,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('properties');
+        Schema::dropIfExists('wishes');
     }
 };
