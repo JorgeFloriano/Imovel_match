8a39989c6e2632a21f612c9c6378d97d0dfbe6b5
Jorge Luis <jorgefloriano1646@gmail.com>
2025-07-19 17:45:55 -0300

    Test errors try fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,22 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Mock Vite for tests
+        app()->bind(\Illuminate\Foundation\Vite::class, function () {
+            return new class {
+                public function __invoke($asset)
+                {
+                    return $asset;
+                }
+                public function __call($name, $arguments)
+                {
+                    return $arguments[0] ?? '';
+                }
+            };
+        });
+    }
 }
